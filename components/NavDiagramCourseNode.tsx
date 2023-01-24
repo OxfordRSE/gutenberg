4a09e6b1739fb0f5066cf301ad6acc71057679eb
martinjrobins <martinjrobins@gmail.com>
2023-01-24 17:26:33 +0000

    style nav diagram

--- components/NavDiagramCourseNode.tsx
+++ components/NavDiagramCourseNode.tsx
@@ -11,10 +11,10 @@ function NavDiagramCourseNode({ data }: NodeProps) {
   console.log(data)
   return (
     <a href={`/material/${data.theme}/${data.course?.id}`} >
-    <div className={`px-4 py-2 h-full w-full border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
+    <div className={`px-4 py-2 h-full w-full border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-600`}>
       <div className="flex">
         <div className="ml-2">
-          <div className="text-lg font-bold">{data.label}</div>
+          <div className="mb-2 text-2xl font-extrabold">{data.label}</div>
         </div>
       </div>
 
--- components/NavDiagramSectionNode.tsx
+++ components/NavDiagramSectionNode.tsx
@@ -9,11 +9,16 @@ type NodeProps = {
 function NavDiagramSectionNode({ data }: NodeProps) {
   return (
     <a href={`/material/${data.theme}/${data.course?.id}/${data.section?.id}`} >
-    <div className={`px-4 py-2 h-full w-full border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`}>
+    <div className={`px-4 py-2 h-full w-full border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-600`}>
       <div className="flex">
         <div className="ml-2">
-          <div className="text-lg font-bold">{data.label}</div>
+          <div className="mb-2 text-xl font-extrabold">{data.label}</div>
         </div>
+      </div>
+      <div className="flex justify-start">
+        {data.section?.tags.map((tag) => (
+            <div className="px-2 pb-1 bg-indigo-500 rounded-lg">{tag}</div>
+        ))}
       </div>
 
       <Handle type="target" position={Position.Top} className="w-16 !bg-teal-500" />
