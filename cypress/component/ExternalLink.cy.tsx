15e540b733ce7beb37b9e059907dd47443915c46
martinjrobins <martinjrobins@gmail.com>
2023-06-29 10:47:35 +0000

    #12 add test ci

---
+++ cypress/component/ExternalLink.cy.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExternalLink from 'components/ui/ExternalLink';
+
+describe('ExternalList', () => {
+  it('renders', () => {
+    cy.mount(
+      <ExternalLink href="https://www.example.com/">test</ExternalLink>
+    )
+  })
+})
