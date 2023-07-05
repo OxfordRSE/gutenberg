9ae7c4915b950c16dc9799d429fe0fb4b9a7dfb1
martinjrobins <martinjrobins@gmail.com>
2023-07-05 07:58:32 +0000

    #19 upgrade yarn

--- components/forms/DateTimeField.tsx
+++ components/forms/DateTimeField.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
-import { Control, Controller, FieldPath, FieldValues } from 'react-hook-form';
+import { Control, Controller, FieldPath, FieldValues, Path, PathValue } from 'react-hook-form';
 import { Label, Select, SelectProps } from 'flowbite-react';
 import DateTimePicker from 'react-datetime-picker';
 import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
+import { Value } from 'react-datetime-picker/dist/cjs/shared/types';
 
 type Props<T extends FieldValues> = {
   label?: string;
@@ -34,7 +35,7 @@ function DateTimeField<T extends FieldValues>({ label, name, control, rules }: P
               id={name}
               name={name}
               className="font-normal  text-gray-700 dark:text-gray-400"
-              onChange={onChange} 
+              onChange={(value: Value) => onChange(value as PathValue<T, Path<T>>)}
               onBlur={onBlur}
               value={value} 
             />
