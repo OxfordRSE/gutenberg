676c2f61fe3a8e153e75d6655597a6c1fe0816ed
Alasdair Wilson <alasdair.wlsn@gmail.com>
2023-08-17 15:58:29 +0100

    changed theme card key to id

--- pages/material/index.tsx
+++ pages/material/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage<HomeProps> = ({ material, events }) => {
 
 function generateThemeCards(material: Material) {
   const cards = material.themes.map((theme, index) => (
-    <Card key={index} href={`${basePath}/material/${theme.id}`} className="w-90%">
+    <Card key={theme.id} href={`${basePath}/material/${theme.id}`} className="w-90%">
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {theme.name}
       </h5>
