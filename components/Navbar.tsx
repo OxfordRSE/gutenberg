ddcdd60453a7bd7b2ba5aa3aa774911620bd295d
Alasdair Wilson <alasdair.wlsn@gmail.com>
2023-08-18 12:24:22 +0100

    add link back to material when deeper in tree

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Dropdown } from 'flowbite-react'
+import { basePath } from 'lib/basePath'
 import { Course, Material, Section, Theme } from 'lib/material'
 import { Event, EventFull } from 'lib/types'
 import { signIn, signOut, useSession } from 'next-auth/react'
@@ -52,6 +53,16 @@ const Navbar: React.FC<Props> = ({ theme, course, section, material, activeEvent
               Home
           </Link>
         </li>
+        {theme && 
+          <li>
+            <div className="flex items-center">
+              <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+              <Link href={`${basePath}/material`} className="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+                  Material
+              </Link>
+            </div>
+          </li>
+        } 
         {theme && 
           <li>
             <div className="flex items-center">
