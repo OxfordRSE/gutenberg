3dba613d54d15b29ebe09c051cab8e77051dfbbf
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2023-10-03 13:27:17 +0100

    swap from img to next/Image

--- components/AttributionDialog.tsx
+++ components/AttributionDialog.tsx
@@ -2,6 +2,7 @@ import { Modal } from 'flowbite-react';
 import { Attribution } from 'lib/material';
 import React, { useEffect } from 'react';
 import { useEdges } from 'reactflow';
+import Image from 'next/image';
 
 interface LicenseProps {
   name: string;
@@ -10,19 +11,19 @@ interface LicenseProps {
 const License: React.FC<LicenseProps> = ({ name }) => {
   if (name === "CC-BY-4.0") {
     return (
-      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
+      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><Image alt="Creative Commons License" src="https://i.creativecommons.org/l/by/4.0/88x31.png" width={88} height={31}/></a>
     )
   } else if (name == "CC-BY-SA-4.0") {
     return (
-      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><Image alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" width={88} height={31}/></a>
     )
   } else if (name == "CC-BY-NC-4.0") {
     return (
-      <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a>
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><Image alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" width={88} height={31}/></a>
     )
   } else if (name == "CC-BY-NC-SA-4.0") {
     return (
-      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><Image alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" width={88} height={31}/></a>
     )
   }  else {
     return (<>unknown</>)
@@ -40,7 +41,7 @@ const CitationList: React.FC<CitationListProps> = ({ citations }) => {
       {citations.map((citation, i) => (
         <div key={i} className="flex items-center space-x-4">
           <a href={citation.url}>
-            <img src={citation.image} alt={citation.citation} className="h-40 w-40" />
+            <Image src={citation.image} alt={citation.citation} className="h-40 w-40" />
           </a>
           <p className="text-lg font-normal text-gray-700 dark:text-gray-400">{citation.citation}</p>
           <License name={citation.license} />
