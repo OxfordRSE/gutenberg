93f8e3704c8e5cc3a3f896994a985d2e1e7cddce
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2023-10-30 11:11:34 +0000

    comments from review

--- prisma/migrations/20231017100244_add_enrolment_key_events/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "enrolKey" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "instructorKey" TEXT NOT NULL DEFAULT '@mHJdxza218sxzd014s8ASDsa0axv0sdfmBJNLM=3asfdnmYHL1234adczcz';
---
+++ prisma/migrations/20231030095951_add_enrolment_keys_to_event/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "enrolKey" TEXT NOT NULL,
+ADD COLUMN     "instructorKey" TEXT NOT NULL;
