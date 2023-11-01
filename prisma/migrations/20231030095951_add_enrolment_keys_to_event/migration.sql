c04df477ddc5bf352e553abd4b324b7656cfeb71
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2023-11-01 15:42:49 +0000

    allow nullable enrolkeys

--- prisma/migrations/20231030095951_add_enrolment_keys_to_event/migration.sql
+++ prisma/migrations/20231030095951_add_enrolment_keys_to_event/migration.sql
@@ -1,3 +1,4 @@
 -- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "enrolKey" TEXT NOT NULL,
-ADD COLUMN     "instructorKey" TEXT NOT NULL;
+ALTER TABLE "Event" 
+ADD COLUMN     "enrolKey" TEXT NULL,
+ADD COLUMN     "instructorKey" TEXT NULL;
