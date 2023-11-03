07b946fd796fd308908ab72a582906794a2da293
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2023-11-03 16:00:05 +0000

    change displa of problems solved for instructors

--- components/EventItemView.tsx
+++ components/EventItemView.tsx
@@ -4,6 +4,8 @@ import { EventFull, Event, Problem } from 'lib/types'
 import { basePath } from 'lib/basePath'
 import { EventItem } from '@prisma/client';
 import { useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import { uniq } from 'cypress/types/lodash';
 
 type EventItemProps = {
   material: Material,
@@ -20,6 +22,7 @@ const EventItemView: React.FC<EventItemProps> = ({ material, item, problems }) =
   let name = `Error: ${item.section}`
   let key = item.id
   let indent = 0
+
 
   let itemProblems: string[] = []
   if (split.length === 3) {
@@ -48,10 +51,24 @@ const EventItemView: React.FC<EventItemProps> = ({ material, item, problems }) =
   let isCompleted = false;
   let completedLabel = '';
 
+  const uniqueUsers = new Set();
+  let uniqueUsersCount = 0;
   if (problems !== undefined && itemProblems.length > 0) {
-    const completedProblems = problems.filter((p) => p.section === item.section && itemProblems.includes(p.tag) && p.complete  && p.userEmail === session?.user?.email);
-    completedLabel = `[${completedProblems.length}/${itemProblems.length}]`;
-    isCompleted = completedProblems.length === itemProblems.length;
+    problems.forEach((problem) => {
+      uniqueUsers.add(problem.userEmail);
+    });
+    uniqueUsersCount = uniqueUsers.size;
+    const completedProblems = problems.filter((p) => p.section === item.section && itemProblems.includes(p.tag) && p.complete);
+    if (uniqueUsersCount > 1) {
+      // if you are an instructor then you get a total count of problems completed / total problems available to all students
+      completedLabel = `[${completedProblems.length}/${itemProblems.length * uniqueUsersCount}]`
+      isCompleted = completedProblems.length === (itemProblems.length * uniqueUsersCount);
+    }
+    else {
+      // if you are a student then you get a count of problems completed / total problems available to just you
+      completedLabel = `[${completedProblems.length}/${itemProblems.length}]`
+      isCompleted = completedProblems.length === itemProblems.length;
+    }
   }
 
 
