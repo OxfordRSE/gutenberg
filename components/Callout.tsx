4af01011fd2a9a9d5a13d95a827c2494b76976c2
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2024-01-16 12:11:48 +0000

    callout heading padding changes

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -6,7 +6,7 @@ interface ChallengeProps {
 
 const Callout: React.FC<ChallengeProps> = ({ content }) => {
   return (
-    <div className="my-4 px-2 border border-gray-200 rounded-lg bg-slate-50 dark:bg-slate-800 dark:border-gray-700">
+    <div className="my-4 px-2 border border-gray-200 rounded-lg bg-slate-50 dark:bg-slate-800 dark:border-gray-700 callout">
       {content}
     </div>
   )
