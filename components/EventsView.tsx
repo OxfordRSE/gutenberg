8ed903b29693ae2fcca62e18639919d4bb1bb5fe
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2024-02-05 10:53:42 +0000

    automatic hiding of older events can view them now with a button

--- components/EventsView.tsx
+++ components/EventsView.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { Material } from "lib/material"
 import { EventFull, Event } from "lib/types"
 import { Button, Timeline } from "flowbite-react"
+import { BiArrowToBottom, BiArrowToTop } from "react-icons/bi"
 import EventActions from "./EventActions"
 import Link from "next/link"
 import useEvents from "lib/hooks/useEvents"
@@ -23,6 +24,9 @@ type EventsProps = {
 const EventsView: React.FC<EventsProps> = ({ material, events }) => {
   // don't show date/time until the page is loaded (due to rehydration issues)
   const [showDateTime, setShowDateTime] = useState(false)
+  const [oldEvents, setOldEvents] = useState(0)
+  const [newEvents, setNewEvents] = useState(0)
+  const [activeEvent] = useActiveEvent()
   const [showDeleteEventModal, setShowDeleteEventModal] = useRecoilState(deleteEventModalState)
   const [deleteEventId, setDeleteEventId] = useRecoilState(deleteEventIdState)
   const [showDuplicateEventModal, setShowDuplicateEventModal] = useRecoilState(duplicateEventModalState)
@@ -30,6 +34,11 @@ const EventsView: React.FC<EventsProps> = ({ material, events }) => {
 
   useEffect(() => {
     setShowDateTime(true)
+    var cutOffDate = new Date()
+    // we automatically hide events older than 2 months
+    cutOffDate.setMonth(cutOffDate.getMonth() - 2)
+    setOldEvents(events.filter((event) => event.start < cutOffDate).length)
+    setNewEvents(events.filter((event) => event.start >= cutOffDate).length)
   }, [])
 
   const { events: currentEvents, mutate } = useEvents()
@@ -41,6 +50,7 @@ const EventsView: React.FC<EventsProps> = ({ material, events }) => {
 
   for (let i = 0; i < events.length; i++) {
     events[i].start = new Date(events[i].start)
+    events[i].end = new Date(events[i].end)
   }
 
   // sort events by start date
@@ -68,48 +78,98 @@ const EventsView: React.FC<EventsProps> = ({ material, events }) => {
     setDuplicateEventId(eventId)
   }
 
+  const loadMoreEvents = () => {
+    setOldEvents(Math.max(oldEvents - 3, 0))
+  }
+
+  const hideMoreEvents = () => {
+    setOldEvents(Math.min(oldEvents + 3, events.length - newEvents))
+  }
+
   return (
     <Timeline>
-      {events.map((event) => {
-        return (
-          <Timeline.Item key={event.id}>
+      <Stack direction="row" className="mb-4">
+        {oldEvents > 0 ? (
+          <Timeline.Item>
             <Timeline.Point />
             <Timeline.Content>
-              <Timeline.Time className="flex justify-between">
-                <Link href={`/event/${event.id}`}>
-                  {showDateTime && event.start.toLocaleString([], { dateStyle: "medium", timeStyle: "short" })}
-                </Link>
-                {isAdmin && (
-                  <Stack direction="row">
-                    <Tooltip title="Duplicate Event">
-                      <MdContentCopy
-                        className="ml-2 inline flex cursor-pointer"
-                        data-cy={`duplicate-event-${event.id}`}
-                        size={18}
-                        onClick={() => openDuplicateEventModal(event.id)}
-                      />
-                    </Tooltip>
-                    <Tooltip title="Delete Event">
-                      <MdDelete
-                        className="ml-2 inline text-red-500 flex cursor-pointer"
-                        data-cy={`delete-event-${event.id}`}
-                        size={18}
-                        onClick={() => openDeleteEventModal(event.id)}
-                      />
-                    </Tooltip>
-                  </Stack>
-                )}
-              </Timeline.Time>
               <Timeline.Title>
-                <Link href={`/event/${event.id}`}>{event.name}</Link>
+                <Tooltip title="Show older events">
+                  <Button color="gray" size="xs" onClick={loadMoreEvents}>
+                    <BiArrowToTop />
+                  </Button>
+                </Tooltip>
               </Timeline.Title>
-              <Timeline.Body>
-                <Link href={`/event/${event.id}`}>{event.summary}</Link>
-              </Timeline.Body>
-              <EventActions event={event} />
             </Timeline.Content>
           </Timeline.Item>
-        )
+        ) : (
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>
+                <div style={{ width: "34px", height: "20px" }}></div>
+              </Timeline.Title>
+            </Timeline.Content>
+          </Timeline.Item>
+        )}
+        {oldEvents < events.length - newEvents && (
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>
+                <Tooltip title="Hide older events">
+                  <Button color="gray" size="xs" onClick={hideMoreEvents}>
+                    <BiArrowToBottom />
+                  </Button>
+                </Tooltip>
+              </Timeline.Title>
+            </Timeline.Content>
+          </Timeline.Item>
+        )}
+      </Stack>
+      {events.map((event, idx) => {
+        // only show events that are not older than 2 months unless they are the active event
+        if (idx - oldEvents >= 0 || event.id === activeEvent?.id) {
+          return (
+            <Timeline.Item key={event.id}>
+              <Timeline.Point />
+              <Timeline.Content>
+                <Timeline.Time className="flex justify-between">
+                  <Link href={`/event/${event.id}`}>
+                    {showDateTime && event.start.toLocaleString([], { dateStyle: "medium", timeStyle: "short" })}
+                  </Link>
+                  {isAdmin && (
+                    <Stack direction="row">
+                      <Tooltip title="Duplicate Event">
+                        <MdContentCopy
+                          className="ml-2 inline flex cursor-pointer"
+                          data-cy={`duplicate-event-${event.id}`}
+                          size={18}
+                          onClick={() => openDuplicateEventModal(event.id)}
+                        />
+                      </Tooltip>
+                      <Tooltip title="Delete Event">
+                        <MdDelete
+                          className="ml-2 inline text-red-500 flex cursor-pointer"
+                          data-cy={`delete-event-${event.id}`}
+                          size={18}
+                          onClick={() => openDeleteEventModal(event.id)}
+                        />
+                      </Tooltip>
+                    </Stack>
+                  )}
+                </Timeline.Time>
+                <Timeline.Title>
+                  <Link href={`/event/${event.id}`}>{event.name}</Link>
+                </Timeline.Title>
+                <Timeline.Body>
+                  <Link href={`/event/${event.id}`}>{event.summary}</Link>
+                </Timeline.Body>
+                <EventActions event={event} />
+              </Timeline.Content>
+            </Timeline.Item>
+          )
+        }
       })}
       {userProfile?.admin && (
         <Timeline.Item>
