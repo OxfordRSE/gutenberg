342c9ce07c8df6031662a0c42baee5b3f54a36ae
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2024-06-03 15:06:19 +0100

    refactor: Add className and style props to SubTitle, Title, and Stack components

--- components/ui/SubTitle.tsx
+++ components/ui/SubTitle.tsx
@@ -2,10 +2,11 @@ import React from "react"
 
 type Props = {
   text: string
+  className?: string
 }
 
 const SubTitle: React.FC<Props> = (props) => {
-  return <p className="text-center text-lg font-bold mb-4">{props.text}</p>
+  return <p className={`text-center text-lg font-bold mb-4 ${props.className}`}>{props.text}</p>
 }
 
 export default SubTitle
