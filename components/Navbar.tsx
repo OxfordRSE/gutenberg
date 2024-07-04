e283a0f327796b39b863ad3e690e80bc18a00e88
Dan Kimberley <danielkimberley@gmail.com>
2024-07-04 12:27:52 +0100

    burger drawer styling improvements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar: React.FC<Props> = ({
   const [drawerOpen, setDrawerOpen] = useState(false)
   const windowSize = useWindowSize()
   const breakpoint = 900
-  const burgerDrawerMaterialMargin = "ml-3"
+  const burgerDrawerMaterialMargin = "ml-9"
   const { data: session } = useSession()
   const ref1 = useRef<HTMLLIElement>(null)
   const ref2 = useRef<HTMLLIElement>(null)
@@ -331,14 +331,22 @@ const Navbar: React.FC<Props> = ({
                           href={`/material`}
                           className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
                         >
-                    
-                        <span className="pb-2">Material</span>
-                    
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 -960 960 960"
+                            className="w-4 h-4 mr-2 mb-2"
+                            fill="currentColor"
+                          >
+                            <path d="M260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z" />
+                          </svg>
+                          <span className="pb-2">Material</span>
                         </Link>
-                    
                       </div>
                     </li>
-                    <li><Divider variant="fullWidth" /></li>
+                    <li>
+                      <Divider variant="fullWidth" />
+                    </li>
                   </ol>
                 )}
                 {theme && (
@@ -370,7 +378,9 @@ const Navbar: React.FC<Props> = ({
                 {section && (
                   <li aria-current="page">
                     <div className="flex items-center">
-                      <span className={`${burgerDrawerMaterialMargin} text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white`}>
+                      <span
+                        className={`${burgerDrawerMaterialMargin} text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white`}
+                      >
                         {section.name}
                       </span>
                     </div>
