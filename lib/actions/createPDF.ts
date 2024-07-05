8fa8045320beeaddf6698cde61336eec4e695900
Dan Kimberley <danielkimberley@gmail.com>
2024-07-05 14:47:12 +0100

    added dark mode

--- lib/actions/createPDF.ts
+++ lib/actions/createPDF.ts
@@ -1,17 +1,19 @@
-const domToPdf = require('dom-to-pdf')
+import { pages } from "next/dist/build/templates/app-page"
+
+const domToPdf = require("dom-to-pdf")
 
 const createPDF = (name: string) => {
-    console.log('imported function: create pdf')
-    const page = document.querySelector('main')
-    const options = {
-        filename: `${name}.pdf`,
-        compression: 'FAST',
-        excludeClassNames: ['absolute'],
-        overrideWidth: 800,
-    }
-    domToPdf(page, options, function() {
-        console.log('pdf created')
-    })
+  console.log("imported function: create pdf")
+  const page = document.querySelector("main")
+  page!.classList.add("dark:bg-gray-800")
+  const options = {
+    filename: `${name}.pdf`,
+    compression: "FAST",
+    excludeClassNames: ["absolute"],
+    overrideWidth: 800,
+  }
+  domToPdf(page, options, function () {})
+  page!.classList.remove("dark:bg-gray-800")
 }
 
 export default createPDF
