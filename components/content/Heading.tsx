22f5c00faa6faeae60a4525fed05bd79ef9deae9
Dan Kimberley <danielkimberley@gmail.com>
2024-07-17 14:48:04 +0100

    added copied notification

--- components/content/Heading.tsx
+++ components/content/Heading.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import CopyToClipboard from "react-copy-to-clipboard"
 import { FaLink } from "react-icons/fa"
 
@@ -11,6 +11,7 @@ interface HeadingProps {
 const Heading: React.FC<HeadingProps> = ({ content, section, tag = "p" }) => {
   const Tag = tag as keyof JSX.IntrinsicElements
   const headingContent = content?.toString().replaceAll(" ", "-")
+  const [isCopied, setIsCopied] = useState(false)
 
   const generateHeadingURL = () => {
     const href: string = typeof window !== "undefined" ? window.location.href.split("#")[0] : ""
@@ -18,7 +19,10 @@ const Heading: React.FC<HeadingProps> = ({ content, section, tag = "p" }) => {
   }
 
   const onCopyHandler = () => {
-    return
+    setIsCopied(true)
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 1500)
   }
 
   return (
@@ -30,6 +34,7 @@ const Heading: React.FC<HeadingProps> = ({ content, section, tag = "p" }) => {
             <FaLink className="group-hover:text-white" />
           </button>
         </CopyToClipboard>
+        {isCopied && <span className="text-xs text-green-500 flex items-center">Copied to clipboard!</span>}
       </Tag>
     </>
   )
