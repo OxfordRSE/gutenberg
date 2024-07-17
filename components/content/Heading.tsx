4ea6d69f1521146f67a75789917c140d7ea76546
Dan Kimberley <danielkimberley@gmail.com>
2024-07-17 13:13:41 +0100

    added generate URL function

--- components/content/Heading.tsx
+++ components/content/Heading.tsx
@@ -10,6 +10,11 @@ const Heading: React.FC<HeadingProps> = ({ content, section, tag = "p" }) => {
   const Tag = tag as keyof JSX.IntrinsicElements
   const headingContent = content?.toString().replaceAll(" ", "-")
 
+  const generateHeadingURL = () => {
+    const href: string = typeof window !== "undefined" ? window.location.href.split("#")[0] : ""
+    return href + "#" + headingContent ?? ""
+  }
+
   return (
     <>
       <Tag id={headingContent}>{content}</Tag>
