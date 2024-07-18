baed3abd5fef2cb3daa335dd858ba0a3863c4598
Dan Kimberley <danielkimberley@gmail.com>
2024-07-18 15:24:16 +0100

    created heading observer hook

---
+++ lib/hooks/useHeadingObserver.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState, useRef } from "react"
+
+export function useHeadingObserver() {
+  const observer = useRef<IntersectionObserver | undefined>()
+  const [activeId, setActiveId] = useState<string | null>(null)
+
+  useEffect(() => {
+    const handleObserver = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry?.isIntersecting) {
+          setActiveId(entry.target.id)
+        }
+      })
+    }
+
+    observer.current = new IntersectionObserver(handleObserver, {
+      rootMargin: "-20% 0% -35% 0px",
+    })
+
+    const headings = document.querySelectorAll("h2, h3, h4, h5, h6")
+    headings.forEach((heading) => observer.current?.observe(heading))
+    return () => observer.current?.disconnect()
+  }, [])
+
+  return { activeId }
+}
