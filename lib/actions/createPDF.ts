dd17ce88004321b920605f992d35663b372e1285
Dan Kimberley <danielkimberley@gmail.com>
2024-07-24 11:13:01 +0100

    add whitespace to top of generated pdf

--- lib/actions/createPDF.ts
+++ lib/actions/createPDF.ts
@@ -5,15 +5,19 @@ const domToPdf = require("dom-to-pdf")
 const createPDF = (name: string) => {
   console.log("imported function: create pdf")
   const page = document.querySelector("main")
+  const whitespace = document.createElement("br")
+  page?.insertBefore(whitespace, page.firstChild);
   page!.classList.add("dark:bg-gray-800")
   const options = {
     filename: `${name}.pdf`,
     compression: "FAST",
     excludeClassNames: ["absolute"],
     overrideWidth: 800,
+    scale: 1
   }
   domToPdf(page, options, function () {})
   page!.classList.remove("dark:bg-gray-800")
+  page?.removeChild(whitespace)
 }
 
 export default createPDF
