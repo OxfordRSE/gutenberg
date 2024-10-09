dd7db97928b5c845ab068096f62b7438c3b46dbc
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2024-10-09 10:31:25 +0100

    fix build errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en-GB">
-      <Head />
+      <Head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.5.2/flowbite.min.css" rel="stylesheet" />
+      </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
         <NextScript />
