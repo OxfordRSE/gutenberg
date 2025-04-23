93ab3e00fadd1aa6a99cf6ba4064e04883667ef6
Alasdair Wilson <alasdair.wlsn@gmail.com>
2025-04-23 12:58:13 +0100

    ci: remove top level await since apparently cypress hates me

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,58 +6,56 @@ import { dirname } from "path"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-export default async function () {
-  const { default: installLogsPrinter } = await import("cypress-terminal-report/src/installLogsPrinter")
+export default defineConfig({
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const { default: installLogsPrinter } = await import("cypress-terminal-report/src/installLogsPrinter")
+      installLogsPrinter(on)
 
-  return defineConfig({
-    env: {
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      on("before:browser:launch", (browser, launchOptions) => {
+        const width = 1920
+        const height = 1080
+
+        if (browser.name === "chrome" && browser.isHeadless) {
+          launchOptions.args.push(`--window-size=${width},${height}`)
+          launchOptions.args.push("--force-device-scale-factor=1")
+        }
+
+        if (browser.name === "electron" && browser.isHeadless) {
+          launchOptions.preferences.width = width
+          launchOptions.preferences.height = height
+        }
+
+        if (browser.name === "firefox" && browser.isHeadless) {
+          launchOptions.args.push(`--width=${width}`)
+          launchOptions.args.push(`--height=${height}`)
+        }
+
+        return launchOptions
+      })
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080,
-    e2e: {
-      setupNodeEvents(on, config) {
-        installLogsPrinter(on)
-
-        on("before:browser:launch", (browser, launchOptions) => {
-          const width = 1920
-          const height = 1080
-
-          if (browser.name === "chrome" && browser.isHeadless) {
-            launchOptions.args.push(`--window-size=${width},${height}`)
-            launchOptions.args.push("--force-device-scale-factor=1")
-          }
-
-          if (browser.name === "electron" && browser.isHeadless) {
-            launchOptions.preferences.width = width
-            launchOptions.preferences.height = height
-          }
-
-          if (browser.name === "firefox" && browser.isHeadless) {
-            launchOptions.args.push(`--width=${width}`)
-            launchOptions.args.push(`--height=${height}`)
-          }
-
-          return launchOptions
-        })
-      },
-      baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3000",
+  },
+  component: {
+    async setupNodeEvents(on, config) {
+      const { default: installLogsPrinter } = await import("cypress-terminal-report/src/installLogsPrinter")
+      installLogsPrinter(on, { printLogsToConsole: "onFail" })
     },
-    component: {
-      setupNodeEvents(on, config) {
-        installLogsPrinter(on, { printLogsToConsole: "onFail" })
-      },
-      devServer: {
-        framework: "next",
-        bundler: "webpack",
-        webpackConfig: {
-          resolve: {
-            alias: {
-              "@components": path.resolve(__dirname, "./src/components"),
-            },
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+      webpackConfig: {
+        resolve: {
+          alias: {
+            "@components": path.resolve(__dirname, "./src/components"),
           },
         },
       },
     },
-  })
-}
+  },
+})
