cf098ca870fe3bf57c0db96b045053cf5d93a04f
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2025-05-30 10:54:03 +0100

    move where flowbite css is imported

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en-GB">
-      <Head>
-        <link href="/styles/flowbite.min.css" rel="stylesheet" />
-      </Head>
+      <Head></Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
         <NextScript />
