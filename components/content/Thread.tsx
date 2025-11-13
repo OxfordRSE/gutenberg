b002d342c1773869e5ad2295f12824fdc89a7860
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-11-13 10:36:14 +0000

    Extract renderPopup into ThreadDialog component

--- components/content/Thread.tsx
+++ components/content/Thread.tsx
@@ -1,25 +1,15 @@
-import { Button, Dropdown, Tooltip } from "flowbite-react"
-import Avatar from "@mui/material/Avatar"
-import { ReactNode, Ref, useCallback, useMemo, useRef, useState } from "react"
+import { Button } from "flowbite-react"
+import { ReactNode, Ref, useRef } from "react"
 import { CommentThread, Comment } from "pages/api/commentThread"
-import { BiCommentCheck, BiCommentDetail, BiDotsVerticalRounded, BiReply } from "react-icons/bi"
-import { MdDelete } from "react-icons/md"
-import { IoClose } from "react-icons/io5"
+import { BiCommentCheck, BiCommentDetail } from "react-icons/bi"
 
-import postComment from "lib/actions/postComment"
 import useCommentThread from "lib/hooks/useCommentThread"
-import deleteCommentThread from "lib/actions/deleteThread"
-import putCommentThread from "lib/actions/putCommentThread"
 import useActiveEvent from "lib/hooks/useActiveEvents"
 import useEvent from "lib/hooks/useEvent"
 import useProfile from "lib/hooks/useProfile"
-import Stack from "components/ui/Stack"
 
-import { GoIssueClosed } from "react-icons/go"
-import { ImEye, ImEyeBlocked } from "react-icons/im"
 import useUser from "lib/hooks/useUser"
-import CommentView from "./Comment"
-import { Provider } from "jotai"
+import ThreadDialog from "./ThreadDialog"
 
 interface TinyButtonProps {
   children: ReactNode
@@ -82,48 +72,28 @@ function useResolveThread(thread: number | CommentThread) {
 }
 
 const Thread = ({ thread, active, setActive, onDelete, finaliseThread }: ThreadProps) => {
-  const { commentThread, threadId, commentThreadIsLoading, isLoading, isPlaceholder, mutate } = useResolveThread(thread)
-  const { user, isLoading: userIsLoading, error: userError } = useUser(commentThread?.createdByEmail)
-  const { userProfile, isLoading: profileLoading, error: profileError } = useProfile()
-  const [activeEvent, setActiveEvent] = useActiveEvent()
-  const [threadEditing, setThreadEditing] = useState(false)
-  const {
-    event: eventData,
-    error: eventError,
-    isLoading: eventIsLoading,
-    mutate: mutateEvent,
-  } = useEvent(activeEvent?.id)
+  const { commentThread, threadId, commentThreadIsLoading, isPlaceholder, mutate } = useResolveThread(thread)
+  const { isLoading: userIsLoading } = useUser(commentThread?.createdByEmail)
+  const { userProfile, isLoading: profileLoading } = useProfile()
+  const [activeEvent] = useActiveEvent()
+
+  const { event: eventData } = useEvent(activeEvent?.id)
 
   const popupRef = useRef<HTMLDialogElement | null>(null) // Reference for the popup element (the comment thread)
-  const triggerRef = useRef<HTMLDivElement | null>(null) // Reference for the trigger (Thread component) in paragraph
   const buttonRef = useRef<HTMLButtonElement | null>(null) // Reference for the button that opens the thread
 
-  const popupPosition = { top: 0, left: 0 }
-
-  // calculate popup position based on viewport width
-  const popupWidth = 355
-  const viewportWidth = window.innerWidth
-
-  let leftPosition = 40
-  if (leftPosition + popupWidth > viewportWidth) {
-    // If the popup overflows the viewport, adjust to the left
-    leftPosition = 0 - popupWidth - 15
-  }
-
-  popupPosition.left = leftPosition
-
-  const handleOpen = useCallback(() => {
+  const handleOpen = () => {
     setActive(true)
     setTimeout(() => {
       // Wait for one render cycle then focus the dialog.
       popupRef.current?.focus()
     }, 0)
-  }, [])
+  }
 
-  const sortedComments = useMemo(() => {
-    if (!commentThread) return []
-    return commentThread.Comment.sort((a, b) => a.index - b.index)
-  }, [commentThread])
+  const handleClose = () => {
+    setActive(false)
+    buttonRef.current?.focus()
+  }
 
   if (!activeEvent) return null
 
@@ -139,211 +109,9 @@ const Thread = ({ thread, active, setActive, onDelete, finaliseThread }: ThreadP
   const canEdit = userProfile?.admin || userProfile?.email === commentThread?.createdByEmail
   const canResolve = userProfile?.admin || isInstructor || userProfile?.email === commentThread?.createdByEmail
 
-  const savePlaceholder = (placeholder: Comment) => {
-    finaliseThread(commentThread, placeholder)
-  }
-
-  const mutateComment = (comment: Comment) => {
-    if (!commentThread) return
-    const updatedComments: Comment[] = commentThread.Comment.map((c) => {
-      if (c?.id === comment.id) {
-        return comment
-      } else {
-        return c as Comment
-      }
-    })
-    mutate({ ...commentThread, Comment: updatedComments })
-  }
-
-  const deleteComment = (comment: Comment) => {
-    if (!commentThread) return
-    mutate({ ...commentThread, Comment: commentThread.Comment.filter((c) => c.id !== comment.id) })
-  }
-
-  const handleClose = () => {
-    setActive(false)
-    buttonRef.current?.focus()
-  }
-
-  const handleDialogKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Escape") {
-      handleClose()
-    }
-  }
-
-  const handleReply = () => {
-    if (!commentThread || isPlaceholder) return
-    postComment(threadId).then((comment: Comment) => {
-      mutate({ ...commentThread, Comment: [...commentThread.Comment, comment] })
-    })
-  }
-
-  const handleDelete = () => {
-    if (!commentThread || isPlaceholder) return
-    deleteCommentThread(commentThread.id).then((commentThread: CommentThread) => {
-      onDelete()
-    })
-  }
-
-  const handleInstructorOnly = () => {
-    if (!commentThread) return
-    putCommentThread({ ...commentThread, instructorOnly: !commentThread.instructorOnly }).then(
-      (commentThread: CommentThread) => {
-        mutate(commentThread)
-      }
-    )
-  }
-
-  const handleResolved = () => {
-    if (!commentThread) return
-    putCommentThread({ ...commentThread, resolved: !commentThread.resolved }).then((commentThread: CommentThread) => {
-      mutate(commentThread)
-    })
-  }
-
-  const renderPopup = () => {
-    return (
-      <dialog
-        aria-label="Comments"
-        open={active}
-        ref={popupRef}
-        className="absolute w-[355px] border border-gray-200 rounded-lg bg-slate-50 dark:bg-slate-900 dark:border-gray-700 z-50"
-        style={{
-          margin: 0,
-          top: `${popupPosition.top}px`,
-          left: `${popupPosition.left}px`,
-        }}
-        data-cy={`Thread:${threadId}:Main`}
-        onKeyDown={handleDialogKeyDown}
-      >
-        <div className="absolute -top-1 left-0 not-prose">
-          <Tooltip
-            content={
-              <>
-                <span className="block text-sm">{user?.name}</span>
-                <span className="block truncate text-sm font-medium">{commentThread?.createdByEmail}</span>
-                <span className="block text-sm">
-                  {commentThread?.created
-                    ? new Date(commentThread?.created).toLocaleString([], { dateStyle: "medium", timeStyle: "short" })
-                    : ""}
-                </span>
-              </>
-            }
-          >
-            <Stack direction="row" spacing={2} className="justify-center">
-              <Avatar sx={{ width: 32, height: 32 }} src={user?.image || undefined} alt={user?.name || "Sign in"} />
-              {commentThread?.resolved === true && (
-                <GoIssueClosed
-                  className="h-6 w-6 font-bold text-green dark:text-green-600"
-                  data-cy={`Thread:${threadId}:IsResolved`}
-                />
-              )}
-            </Stack>
-          </Tooltip>
-        </div>
-        <div className={`flex items-center justify-between rounded-t-lg`}>
-          <h3 className="w-full mx-2 my-0 text-base text-slate-100 dark:text-slate-900">{}</h3>
-          <Stack direction="row-reverse">
-            <TinyButton onClick={handleClose} dataCy={`Thread:${threadId}:CloseButton`} aria-label="Close comments">
-              <IoClose />
-            </TinyButton>
-            {!isPlaceholder && (
-              <Dropdown
-                renderTrigger={() => (
-                  <TinyButton>
-                    <BiDotsVerticalRounded className="h-4 w-4" data-cy={`Thread:${threadId}:Dropdown`} />
-                  </TinyButton>
-                )}
-                label={undefined}
-                className="not-prose"
-                aria-label="Thread options"
-              >
-                <Dropdown.Item
-                  className="hover:bg-gray-400"
-                  icon={commentThread?.instructorOnly === true ? ImEyeBlocked : ImEye}
-                  onClick={handleInstructorOnly}
-                  data-cy={`Thread:${threadId}:Visibility`}
-                >
-                  Visibility
-                </Dropdown.Item>
-                <Dropdown.Item
-                  className="hover:bg-gray-400"
-                  icon={MdDelete}
-                  onClick={handleDelete}
-                  data-cy={`Thread:${threadId}:Delete`}
-                >
-                  Delete
-                </Dropdown.Item>
-              </Dropdown>
-            )}
-          </Stack>
-        </div>
-        {isPlaceholder &&
-          sortedComments.map((comment) => (
-            <Provider key={comment.id}>
-              <CommentView
-                key={comment.id}
-                comment={comment}
-                mutateComment={savePlaceholder}
-                saveComment={savePlaceholder}
-                deleteComment={deleteComment}
-                isPlaceholder={true}
-                threadEditing={threadEditing}
-                setThreadEditing={setThreadEditing}
-              />
-            </Provider>
-          ))}
-        {!isPlaceholder &&
-          sortedComments.map((comment) => (
-            <Provider key={comment.id}>
-              <CommentView
-                key={comment.id}
-                comment={comment}
-                mutateComment={mutateComment}
-                deleteComment={deleteComment}
-                isPlaceholder={false}
-                threadEditing={threadEditing}
-                setThreadEditing={setThreadEditing}
-              />
-            </Provider>
-          ))}
-        {!isPlaceholder && (
-          <Stack direction="row-reverse">
-            {canResolve && (
-              <Tooltip content="Mark as resolved" placement="top">
-                <TinyButton
-                  onClick={handleResolved}
-                  dataCy={`Thread:${threadId}:Resolved`}
-                  aria-label="Mark as resolved"
-                >
-                  {commentThread?.resolved ? (
-                    <GoIssueClosed className="h-4 w-4 text-green dark:text-green-600" />
-                  ) : (
-                    <GoIssueClosed className="h-4 w-4" />
-                  )}
-                </TinyButton>
-              </Tooltip>
-            )}
-            {!threadEditing && (
-              <Tooltip content="Reply in thread" placement="top">
-                <TinyButton
-                  onClick={handleReply}
-                  aria-label="Reply in thread"
-                  dataCy={`Thread:${threadId}:ReplyButton`}
-                >
-                  <BiReply className="h-4 w-4" data-cy={`Thread:${threadId}:Reply`} />
-                </TinyButton>
-              </Tooltip>
-            )}
-          </Stack>
-        )}
-      </dialog>
-    )
-  }
-
   return (
     <div className="relative" id={`comment-thread-${threadId}`}>
-      <div className="flex justify-end opacity-50 md:opacity-100 xl:justify-start" ref={triggerRef}>
+      <div className="flex justify-end opacity-50 md:opacity-100 xl:justify-start">
         <TinyButton
           ref={buttonRef}
           onClick={handleOpen}
@@ -359,7 +127,19 @@ const Thread = ({ thread, active, setActive, onDelete, finaliseThread }: ThreadP
           )}
         </TinyButton>
       </div>
-      {renderPopup()}
+      <ThreadDialog
+        active={active}
+        setActive={setActive}
+        canEdit={canEdit}
+        canResolve={canResolve}
+        commentThread={commentThread}
+        finaliseThread={finaliseThread}
+        handleClose={handleClose}
+        isPlaceholder={isPlaceholder}
+        mutate={mutate}
+        onDelete={onDelete}
+        ref={popupRef}
+      />
     </div>
   )
 }
