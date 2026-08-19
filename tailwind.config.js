885aa426a39ea958dd2c7e580f8b798139da664c
Alasdair Wilson <alasdair.wlsn@googlemail.com>
2026-08-19 11:26:42 +0100

    bump flowbite for brace expansion dep

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
     require("flowbite/plugin"),
   ],
 }
